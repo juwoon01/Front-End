561211b912ee711ef4c4b93ba2ea863b796024cd
DESKTOP-KD5JI4A\juwon <juwoon01@naver.com>
2022-06-06 18:48:30 +0900

    feat: Add useLocation

--- src/components/main/Home/index.tsx
+++ src/components/main/Home/index.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import styled from 'styled-components';
 import Header from '../../organisms/Header';
+import { useLocation } from 'react-router-dom';
 
 const Home = ()=>{
+    const location = useLocation();
+    let isName;
+    if(location.state != null){
+        const state = location.state as {isName : string};
+        isName = state.isName;
+    }else{
+        isName = '로그인';
+    }
+
     return(
         <HomeWrrapper>
-            <Header name={'로그인'}/>
+            <Header name={isName}/>
+            <div>main page</div>
         </HomeWrrapper>
     );
 }
